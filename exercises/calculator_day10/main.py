05123aa71fdf999ef84ba6d4c5cacc5ae9ce9cf3
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-03-25 10:16:23 -0400

    Update main.py
    basic calculator functions

--- exercises/calculator_day10/main.py
+++ exercises/calculator_day10/main.py
@@ -1,1 +1,20 @@
 #calculator
+
+#add
+def add(n1, n2):
+    return n1 + n2
+
+#subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#divide
+def divide(n1, n2):
+    if n2 == 0:
+        return "Can't divide by zero"
+    else:
+        return n1 / n2
