91f446661439269bba27905ca95b3f06629d7293
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-03-25 15:14:06 -0400

    Update main.py
    updated cards and deal card function

--- blackjack/main.py
+++ blackjack/main.py
@@ -31,10 +31,13 @@
 #Hint 3: Download and read this flow chart I've created: 
 #   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
 
+import random 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card():
+    choice = random.randint(0,12)
+    return cards[choice]
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 #user_cards = []
 #computer_cards = []
