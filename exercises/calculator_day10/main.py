3ec959b602a0ae909aa733da0739397b8814697b
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-03-25 10:42:27 -0400

    added clear function for console

--- exercises/calculator_day10/main.py
+++ exercises/calculator_day10/main.py
@@ -1,5 +1,5 @@
 #calculator
-
+from clear import clearscreen
 #add
 def add(n1, n2):
     return n1 + n2
@@ -26,22 +26,29 @@ operations = {
     "/" : divide
 }
 
+def calculator():
+    num1 = int(input("What is your first number?: "))
+    num2 = int(input("what is your second number?: "))
+    for symbol in operations:
+        print(symbol)
 
-num1 = int(input("What is your first number?: "))
-num2 = int(input("what is your second number?: "))
-for symbol in operations:
-    print(symbol)
+    operation_reqd = input("Pick an operation from the line above: ")
 
-operation_reqd = input("Pick an operation from the line above: ")
+    to_continue = 'y'
+    while to_continue == 'y':
+        function = operations[operation_reqd]
+        answer = function(num1, num2)
+        print(f"{num1} {operation_reqd} {num2} = {answer}")
 
-to_continue = 'y'
-while to_continue == 'y':
-    function = operations[operation_reqd]
-    answer = function(num1, num2)
-    print(f"{num1} {operation_reqd} {num2} = {answer}")
+        to_continue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit, or 'r' to start a new calculation.: ")
+        if to_continue == 'y':
+            operation_reqd = input("Pick an operation from the line above: ")
+            num1 = answer
+            num2 = int(input("What's the next number?: "))
+        elif to_continue == 'r':
+            clearscreen()
+            calculator()
+        else: 
+            clearscreen()
 
-    to_continue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
-    if to_continue == 'y':
-        operation_reqd = input("Pick an operation from the line above: ")
-        num1 = answer
-        num2 = int(input("What's the next number?: "))
+calculator()
