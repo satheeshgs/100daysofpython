1b18b96318ce28e6baf78b2a2093e9bf047323a7
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-03-26 20:07:45 -0400

    Update main.py
    completed number guessing game

--- number_guessing/main.py
+++ number_guessing/main.py
@@ -5,8 +5,17 @@ import random
 import time
 game_mode = {'h': 5, 'e': 10}
 
+def guess_check(num1, num2):
+    if num1 > num2:
+        return "Too High"
+    elif num1 < num2:
+        return "Too Low"
+    else:
+        return "Correct"
+
 to_continue = 'y'
 while to_continue == 'y':
+    clearscreen()
     print(logo)
     time.sleep(1)
     choice = input("Which mode do you want to play? Enter 'h' for hard mode (5 guesses) or 'e' for easy mode (10 guesses): ")
@@ -15,7 +24,20 @@ while to_continue == 'y':
     
     game_over =  False
     while game_over == False:
-        print(f"You have {number_of_guesses} left.")
-        user_guess = input("Guess your number: ")
+        print(f"You have {number_of_guesses} guesses left.")
+        user_guess = int(input("Guess your number: "))
+
+        while guess_check(user_guess, computer_guess) != "Correct":
+            print(guess_check(user_guess, computer_guess))
+            number_of_guesses -= 1
+            print(f"You have {number_of_guesses} guesses left.")
+            user_guess = int(input("Guess your number: "))
         
-        to_continue = 'n'
+        if guess_check(user_guess, computer_guess) == "Correct":
+            print(f"Congratulations. You guessed correctly. The number is {user_guess}")
+            break
+        else:
+            print("You ran out of guesses.")
+            break
+
+    to_continue = input("Do you want to start a new game? Type 'y' for yes and 'n' for no: ")
