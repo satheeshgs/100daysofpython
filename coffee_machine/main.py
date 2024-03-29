f3bf06f0132c02c85478fd220983c8fb9bc1ec89
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-03-29 18:28:37 -0400

    updated and segregated functions

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -1,3 +1,6 @@
+from art import logo
+from clear import clearscreen
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -42,6 +45,7 @@ def report():
     """
     prints list of resources in coffee machine
     """
+    print("Machine Report")
     print(f"Water: {resources['water']} ml")
     print(f"Milk: {resources['milk']} ml")
     print(f"Coffee: {resources['coffee']} ml")
@@ -49,30 +53,59 @@ def report():
 
 
 def check_water(coffee_type):
+    """
+    check for water in the resources and respond with true or false
+    """
     global resources
     global MENU
     return (resources['water'] > MENU[coffee_type]["ingredients"]['water']) 
 
 def check_milk(coffee_type):
+    """
+    check for milk in the resources and respond with true or false
+    """
     global resources
     global MENU
+    if coffee_type == "espresso":
+        return True
     return (resources['milk'] > MENU[coffee_type]["ingredients"]['milk']) 
 
 def check_coffee(coffee_type):
+    """
+    check for coffee in the resources and respond with true or false
+    """
     global resources
     global MENU
     return (resources['coffee'] > MENU[coffee_type]["ingredients"]['coffee']) 
 
 def calculate_money(quarters, dimes, nickles, pennies):
+    """
+    calculate money inserted by user and returns amount
+    """
     return 0.25*quarters + 0.1*dimes + 0.05*nickles + 0.01*pennies
 
 def calculate_change(money_inserted, coffee_type):
+    """
+    checks money inserted and returns change based on coffee type
+    """
     return money_inserted - MENU[coffee_type]['cost']
 
-report()
-
+def update_resources(coffee_type):
+    """
+    updates resources based on coffee type
+    """
+    if coffee_type != "espresso":
+        resources['milk'] -= MENU[coffee_type]["ingredients"]['milk']
+    resources['coffee'] -= MENU[coffee_type]["ingredients"]['coffee']
+    resources['water'] -= MENU[coffee_type]["ingredients"]['water']
+    resources['money'] += MENU[coffee_type]["cost"]
+    return True
 
 def make_coffee():
+    """
+    Make a coffee - espresso/latte/cappuccino based on resource availability
+    """
+
     coffee_type = input("What would you like? (espresso/latte/cappuccino): ")
     
     #secret turn off machine
@@ -82,10 +115,7 @@ def make_coffee():
         return
     
     #checking all resources and giving the user messages
-    if coffee_type != "espresso":
-        milk_available = check_milk(coffee_type)
-    else:
-        milk_available = True
+    milk_available = check_milk(coffee_type)
     
     if not milk_available: 
         print("Sorry there is not enough milk. Exiting")
@@ -112,11 +142,9 @@ def make_coffee():
             return
         else:
             change = calculate_change(money_inserted, coffee_type)
-            if coffee_type != "espresso":
-                resources['milk'] -= MENU[coffee_type]["ingredients"]['milk']
-            resources['coffee'] -= MENU[coffee_type]["ingredients"]['coffee']
-            resources['water'] -= MENU[coffee_type]["ingredients"]['water']
-            resources['money'] += cost
+            update_resources(coffee_type)
+            clearscreen()
+            print(logo)
             print(f"Here is your {coffee_type}. Enjoy!")
             print(f"Here is ${change} dollars in change")
 
