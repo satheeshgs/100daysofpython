a00ab5844b69cd567875da368020221ecafda5de
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-03-29 21:20:26 -0400

    Turtle graphics exploration

---
+++ turtle_graphics/main.py
@@ -0,0 +1,11 @@
+#Turtle graphics
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("triangle")
+timmy.color("PeachPuff")
+timmy.pencolor("DarkOrchid")
+timmy.forward(100)
+
+my_screen = Screen()
+my_screen.exitonclick()
