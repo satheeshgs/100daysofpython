a2c2203c6a27d78e6696cb39fa70df60aee76372
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-03-29 22:41:46 -0400

    updated starting files

--- coffee_machine_oops/main.py
+++ coffee_machine_oops/main.py
@@ -2,3 +2,10 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee = CoffeeMaker()
+money = MoneyMachine()
+coffee_menu = Menu()
+
+user_choice = input("What is your drink? espresso/cappuccino/latte? ")
+print(coffee_menu.find_drink(user_choice))
+print(coffee.is_resource_sufficient(user_choice))
