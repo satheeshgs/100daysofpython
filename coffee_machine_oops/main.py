f75e08b6ba38724875297d35a89f1cd5c7f412de
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-03-29 23:09:07 -0400

    Update main.py
    minor updates to the coffee machine OOPS project

--- coffee_machine_oops/main.py
+++ coffee_machine_oops/main.py
@@ -5,18 +5,20 @@ from money_machine import MoneyMachine
 coffee = CoffeeMaker()
 money = MoneyMachine()
 coffee_menu = Menu()
+is_on = True
 
-user_choice = input(f"What is your drink? {coffee_menu.get_items()} ")
-if user_choice == 'report':
-    coffee.report()
-else:
-    drink = coffee_menu.find_drink(user_choice) #user's choice of drink
-
-    resource_check = coffee.is_resource_sufficient(drink)
-    if not resource_check:
-        print(resource_check)
+while is_on:
+    options = coffee_menu.get_items()
+    user_choice = input(f"What is your drink? {options} ")
+    if user_choice == 'off':
+        is_on = False
+    elif user_choice == 'report':
+        coffee.report()
+        money.report()
     else:
+        drink = coffee_menu.find_drink(user_choice) #user's choice of drink
         cost = drink.cost
-        if money.make_payment(cost): 
+        if coffee.is_resource_sufficient(drink) and money.make_payment(cost): 
             coffee.make_coffee(drink)
         coffee.report()
+        money.report()
