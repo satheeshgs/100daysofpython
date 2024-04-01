f47fcc361591bd8791e9857c3e1fa1a2a972ac82
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-01 18:11:38 -0400

    minor updates

--- turtle_graphics/fun_exploration.py
+++ turtle_graphics/fun_exploration.py
@@ -2,14 +2,11 @@
 from turtle import Turtle, Screen
 from prettytable import PrettyTable
 
-timmy = Turtle()
-timmy.shape("triangle")
-timmy.color("PeachPuff")
-timmy.pencolor("DarkOrchid")
-timmy.forward(100)
-
-my_screen = Screen()
-my_screen.exitonclick()
+jfk = Turtle()
+jfk.shape("triangle")
+jfk.color("PeachPuff")
+jfk.pencolor("DarkOrchid")
+jfk.forward(100)
 
 table = PrettyTable()
 print(table)
@@ -18,3 +15,38 @@ table.add_column("Type", ["Electric", "Water", "Fire"])
 table.add_row(["Psyduck", "Psychic"])
 table.align = 'l'
 print(table)
+
+
+def draw_square(turtle):
+    for i in range(4):
+        turtle.forward(100)
+        turtle.right(90)
+
+jfk.shape('triangle')
+jfk.color('DarkSalmon')
+jfk.pencolor('Maroon')
+
+for i in range(5):
+    jfk.down()
+    jfk.forward(10)
+    jfk.up()
+    jfk.forward(10)
+
+#draw_square(jfk)
+
+#different shapes
+jfk.down()
+
+def draw_shapes(sides):
+
+    for _ in range(sides):
+        angle = 360/ sides
+        jfk.forward(20)
+        jfk.right(angle)
+
+for i in range(3,11):
+    draw_shapes(i)
+
+
+my_screen = Screen()
+my_screen.exitonclick()
--- turtle_graphics/main.py
+++ turtle_graphics/main.py
@@ -2,22 +2,9 @@
 from turtle import Turtle, Screen
 
 jfk = Turtle()
-jfk.shape('triangle')
-jfk.color('DarkSalmon')
-jfk.pencolor('Maroon')
 
-def draw_square(turtle):
-    for i in range(4):
-        turtle.forward(100)
-        turtle.right(90)
 
-for i in range(5):
-    jfk.down()
-    jfk.forward(10)
-    jfk.up()
-    jfk.forward(10)
 
-#draw_square(jfk)
 
 
 
