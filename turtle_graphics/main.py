13bcc6d69694988ff17679198c121703c8d4f6a5
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-01 07:28:40 -0400

    Update main.py

--- turtle_graphics/main.py
+++ turtle_graphics/main.py
@@ -1,8 +1,36 @@
 #hirst painting
 from turtle import Turtle, Screen
 
-arcadia = Turtle()
-arcadia.shape('triangle')
+jfk = Turtle()
+jfk.shape('triangle')
+jfk.color('DarkSalmon')
+jfk.pencolor('Maroon')
+
+def draw_square(turtle):
+    for i in range(4):
+        turtle.forward(100)
+        turtle.right(90)
+
+for i in range(5):
+    jfk.down()
+    jfk.forward(10)
+    jfk.up()
+    jfk.forward(10)
+
+#draw_square(jfk)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 my_screen = Screen()
 my_screen.exitonclick()
