c4cacfdd1c4d01f36a9feebbf4065f3d626f60e6
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-02 08:17:22 -0400

    Create random_walk.py
    random walk generator

---
+++ turtle_graphics/random_walk.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+import random
+
+scene = Screen()
+scene.colormode(255)
+
+jfk = Turtle()
+jfk.pensize(15)
+jfk.speed("fastest") #use 0 for numerical for fastest
+
+directions = [0, 90, 180, 270]
+
+for i in range(0,100):
+    red = random.randint(0,255)
+    green = random.randint(0,255)
+    blue = random.randint(0,255)
+    jfk.pencolor(red, green, blue) #choosing a random pen color
+    jfk.forward(25)
+    direction = random.choice(directions)
+    jfk.setheading(direction)
+
+
+
+
+
+
+scene.exitonclick()
