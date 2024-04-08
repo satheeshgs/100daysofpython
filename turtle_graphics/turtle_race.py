9c7c54f4c389a16decae6a330cf6868f8925162c
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-07 23:20:51 -0400

    Update turtle_race.py

--- turtle_graphics/turtle_race.py
+++ turtle_graphics/turtle_race.py
@@ -1,6 +1,17 @@
 from turtle import Turtle, Screen
 import random
 
-scene = Screen()
-print(scene.screensize())
-scene.exitonclick()
+screen = Screen()
+screen.setup(width=500,height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which color turtle will win? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+ypos = [-80, -50, -20, 10, 40, 70]
+
+#set turtles to race
+for i in range(len(colors)):
+    jfk = Turtle(shape="turtle")
+    jfk.color(colors[i])
+    jfk.up()
+    jfk.goto(-230,ypos[i])
+
+screen.exitonclick()
