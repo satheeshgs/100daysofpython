d7638523454af4e2008aa1e7a0be3e110fecf066
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-13 12:55:40 -0400

    Food collision detected in class

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600,height=600)
@@ -8,13 +9,14 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-jfk = Snake()
+snake = Snake()
+food = Food()
 
 screen.listen()
-screen.onkey(jfk.up, "Up")
-screen.onkey(jfk.down, "Down")
-screen.onkey(jfk.left, "Left")
-screen.onkey(jfk.right, "Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 is_game_on = True
 
@@ -23,7 +25,12 @@ while is_game_on:
     time.sleep(0.1)
 
     #moving the snake forward by moving the last segment to move forward to the previous segment 
-    jfk.move()
+    snake.move()
+
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
 
 
 
