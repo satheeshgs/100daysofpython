1c7cfc668656c6f52739c7a8e439cf89776e61d6
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-14 06:35:27 -0400

    detect collision with tail

--- snake_game/main.py
+++ snake_game/main.py
@@ -41,7 +41,13 @@ while is_game_on:
         scoreboard.game_over()
 
 
-
+    #detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
