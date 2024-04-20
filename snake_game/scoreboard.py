195c33dd6def7d49fe19ce3c7e22d35b7fbab0e7
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-20 16:53:39 -0400

    setting high score using files - snake game

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,7 +9,7 @@ class Scoreboard(Turtle):
 
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_high_score()
         self.initial_setup()
         self.print_score()
         
@@ -17,10 +17,19 @@ class Scoreboard(Turtle):
         self.goto(SCOREBOARD_POSITION)
         self.hideturtle()
         self.pencolor("white")
-    
+
+    def get_high_score(self):
+        with open("high_score.txt") as file:
+            high_score = file.read()
+        return high_score
+
+    def set_high_score(self, score):
+        with open("high_score.txt", mode='w') as file:
+            file.write(f"{score}")    
 
     def print_score(self):
         self.clear()
+        self.high_score = self.get_high_score()
         self.write(f"Score: {self.score}  High Score: {self.high_score}", False, align=ALIGNMENT, font=(FONT, 14, 'normal'))
 
     def increase_score(self):
@@ -32,7 +41,7 @@ class Scoreboard(Turtle):
     #    self.write(f"GAME OVER", False, align=ALIGNMENT, font=(FONT, 16, 'normal'))
         
     def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
+        if self.score > int(self.high_score):
+            self.set_high_score(self.score)
         self.score = 0
         self.print_score()
