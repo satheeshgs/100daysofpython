681b9602de45f4005e5b5456793db3493dc104cd
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-22 11:21:02 -0400

    squirrel data grouped

--- pandas_experimentation/main.py
+++ pandas_experimentation/main.py
@@ -1,3 +1,6 @@
 import pandas as pd
 data = pd.read_csv("2018_Central_Park_squirrel_data.csv")
-print(data.head(5))
+#print(data.columns)
+
+data_new = data.groupby("Primary Fur Color").size()
+data_new.to_csv("squirrel_data_grouped.csv")
