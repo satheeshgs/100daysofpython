cf2d989f49542abda199de4eccb09cdf8c5abfaf
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-23 07:00:04 -0400

    updates to US states game

--- name_the_us_states/main.py
+++ name_the_us_states/main.py
@@ -8,15 +8,20 @@ screen.addshape(image)
 turtle.shape(image)
 
 states_data = pd.read_csv("50_states.csv")
+all_states = states_data.state.to_list()
 
 game_is_on = True
 
 while game_is_on:
     user_guess = screen.textinput(title="Guess the states", prompt="What's the state name that you are guessing?")
     user_guess = user_guess.title()
-    user_guess_x = states_data.loc[states_data.state == user_guess, 'x'].iloc[0]  #find user guess x value
-    user_guess_y = states_data.loc[states_data.state == user_guess, 'y'].iloc[0]  #find user guess y value
-    turtle.setposition(user_guess_x, user_guess_y)
-    turtle.write(user_guess)  #display user guess in (x,y) location
+    if user_guess in all_states:
+        user_guess_x = states_data.loc[states_data.state == user_guess, 'x'].iloc[0]  #find user guess x value
+        user_guess_y = states_data.loc[states_data.state == user_guess, 'y'].iloc[0]  #find user guess y value
+        state_turtle = turtle.Turtle() #create a new turtle on the screen to move to user location
+        state_turtle.hideturtle()
+        state_turtle.penup()
+        state_turtle.goto(user_guess_x, user_guess_y)
+        state_turtle.write(user_guess)  #display user guess in (x,y) location
 
 turtle.mainloop()
