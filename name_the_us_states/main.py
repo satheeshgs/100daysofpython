8a357c1d04893a2322e3fc077257c049f784d582
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-23 07:38:58 -0400

    created an unguessed states file for user

--- name_the_us_states/main.py
+++ name_the_us_states/main.py
@@ -17,6 +17,8 @@ while len(guessed_states) < 50:
     user_guess = screen.textinput(title=f"{user_score}/50 correct. Guess the states", prompt="What's the state name that you are guessing?")
     user_guess = user_guess.title()
     guessed_states.append(user_guess)
+    if user_guess == "Exit": #exit the game if required
+        break
     if user_guess in all_states:
         user_guess_x = states_data.loc[states_data.state == user_guess, 'x'].iloc[0]  #find user guess x value
         user_guess_y = states_data.loc[states_data.state == user_guess, 'y'].iloc[0]  #find user guess y value
@@ -26,5 +28,14 @@ while len(guessed_states) < 50:
         state_turtle.penup()
         state_turtle.goto(user_guess_x, user_guess_y)
         state_turtle.write(user_guess)  #display user guess in (x,y) location
+
+if user_guess == "Exit":
+    unguessed_states = []
+    for state in all_states:
+        if state not in guessed_states:
+            unguessed_states.append(state)
+
+    df = pd.DataFrame(unguessed_states, columns=["state"])
+    df.to_csv("unguessed_states.csv", index=False)
 
 turtle.mainloop()
