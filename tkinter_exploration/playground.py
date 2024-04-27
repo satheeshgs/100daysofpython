173aa921b5bf68ff927bd17ef270a965de372890
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-27 11:26:18 -0400

    keyword arguments in python

--- tkinter_exploration/playground.py
+++ tkinter_exploration/playground.py
@@ -5,3 +5,26 @@ def add(*args):
     return total
 
 print(add(576,829,192,223))
+
+
+def calculate(n, **kwargs): #keyword arguments
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kwargs):
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+        self.color = kwargs.get("color")
+        self.max_speed = kwargs.get("speed")
+        self.type = kwargs.get("type")
+
+my_car = Car(make="Honda", model="Jazz", color="Red Pearl")
+
+print(my_car.make)
