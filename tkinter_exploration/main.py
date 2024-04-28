239f302c01f43a410aea723728138ddfd490edbe
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-04-28 07:54:44 -0400

    tkinter explorations

--- tkinter_exploration/main.py
+++ tkinter_exploration/main.py
@@ -1,16 +1,27 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI")
 window.minsize(width=500, height=300)
 
 #Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24))
+my_label = Label(text="I am a label", font=("Arial", 24))
 my_label.pack(side="top")
 
 
+my_label.config(text="New text") #using config of the tcl commands
+
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text="I got clicked")
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
 
 
+#entry
+input = Entry()
+input.pack()
 
 
 window.mainloop()
