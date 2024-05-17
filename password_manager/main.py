f5a1f753fe521bfd0bdbfecbdd7d9adb0b14cbdc
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-05-17 10:52:31 -0400

    mypass - password manager start

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,5 +1,61 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+
+    with open("data.txt", "a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+        website_input.delete(0, END)
+        password_input.delete(0, END)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+
+canvas = Canvas(width=200, height=200, highlightthickness=2)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(column=1, row=0)
+
+
+#labels
+website_label = Label(text="Website: ")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/ Username: ")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password: ")
+password_label.grid(column=0, row=3)
+
+#entries
+website_input = Entry(width=35)
+website_input.grid(row=1, column=1, columnspan=2)
+website_input.focus()
+
+email_input = Entry(width=35)
+email_input.grid(row=2, column=1, columnspan=2)
+email_input.insert(0, "a default value")
+
+password_input = Entry(width=21)
+password_input.grid(row=3, column=1)
+
+#buttons
+generate_pwd_button = Button(text="Generate Password")
+generate_pwd_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+window.mainloop()
