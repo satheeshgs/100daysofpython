20b245768e501e91ab8cd2ea669d6a5cc57f1148
Satheesh Gowtham Sundararajan <satheesh.gowtham@gmail.com>
2024-07-08 22:56:53 -0400

    day 34 - quizzler completed

--- day34-quizzler-app/main.py
+++ day34-quizzler-app/main.py
@@ -12,7 +12,7 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz_ui = QuiZInterface()
+quiz_ui = QuiZInterface(quiz)
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
